06a32cf2d6ee5eacde18df5586aa6c967a296f9a
Ezee1234 <akriti09bhardwaj@gmail.com>
2024-01-24 16:27:38 +0530

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using AspCoreWebApiCRUD_Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Policy;
+using System.Text;
+
+namespace AspCoreWebApiCRUD_Client.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string url = "https://localhost:7034/api/StudentAPI/";
+        private HttpClient client = new HttpClient();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>();
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<List<Student>>(result);
+                if (data != null)
+                {
+                    students = data;
+                }
+            }
+            return View(students);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student std)
+        {
+            string data = JsonConvert.SerializeObject(std);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if(response.IsSuccessStatusCode)
+            {
+                TempData["insert_message"] = "student Added..";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspCoreWebApiCRUD_Client.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+
+        [Required]
+        public string studentName { get; set; }
+
+        [Required]
+        public string studentGender { get; set; }
+
+        [Required]
+        public int age { get; set; }
+
+        [Required]
+        public int standard { get; set; }
+    }
+
+}
